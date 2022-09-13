2a47f23ec06d8920f1c793e396b8da33606aea0f
Award <awardmalisi@gmail.com>
2022-09-13 18:38:12 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import "./sass/main.scss";
+
+const secondHand = document.querySelector(".second-hand");
+const minsHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  const seconds: number = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  //rotate(X degrees)
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+//This invokes the setDate() every 1000 milliseconds. 
+
+setDate();
