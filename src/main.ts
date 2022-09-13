b415f170948cd8dec4b31e0e9796815dcd15f153
Award <awardmalisi@gmail.com>
2022-09-13 18:56:16 -0500

    I want Jet's Pizza

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
 import "./sass/main.scss";
 
-const secondHand = document.querySelector(".second-hand");
-const minsHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector(".hour-hand");
+// type num = number;
+
+const secondHand = document.querySelector<HTMLElement>(".second-hand");
+const minsHand = document.querySelector<HTMLElement>(".min-hand");
+const hourHand = document.querySelector<HTMLElement>(".hour-hand");
 
 function setDate() {
   const now = new Date();
 
-  const seconds: number = now.getSeconds();
+  const seconds = now.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  secondHand!.style.transform = `rotate(${secondsDegrees}deg)`;
   //rotate(X degrees)
 
   const mins = now.getMinutes();
   const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  minsHand!.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hour = now.getHours();
   const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  hourHand!.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
-//This invokes the setDate() every 1000 milliseconds. 
-
-setDate();
+//This invokes the setDate() every 1000 milliseconds.
